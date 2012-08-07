c9faeae30f5e8f9036d60906b5a309f1b70c6523
Eugene Munyaneza <munyanezae@gmail.com>
2012-08-07 16:17:09 +0200

    added controllers test for the department. good handling of i18n fields

---
+++ test/integration/org/chai/memms/equipment/EquipmentStatusControllerSpec.groovy
@@ -0,0 +1,7 @@
+package org.chai.memms.equipment
+
+import org.chai.memms.IntegrationTests;
+
+class EquipmentStatusControllerSpec extends IntegrationTests{
+
+}
---
+++ test/integration/org/chai/memms/equipment/WarrantyControllerSpec.groovy
@@ -0,0 +1,7 @@
+package org.chai.memms.equipment
+
+import org.chai.memms.IntegrationTests;
+
+class WarrantyControllerSpec extends IntegrationTests{
+
+}
