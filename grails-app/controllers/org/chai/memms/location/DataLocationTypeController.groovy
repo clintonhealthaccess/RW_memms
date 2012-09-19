da6b005144a36a1ae7382846e5adc9c9cedf7d46
Eugene Munyaneza <munyanezae@gmail.com>
2012-09-19 16:56:53 +0200

    fixed most error messages for the equipment and actions under admin

--- grails-app/controllers/org/chai/memms/location/DataLocationTypeController.groovy
+++ grails-app/controllers/org/chai/memms/location/DataLocationTypeController.groovy
@@ -25,8 +25,9 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.chai.location;
+package org.chai.memms.location;
 
+import org.chai.location.DataLocationType;
 import org.chai.memms.AbstractEntityController;
 
 class DataLocationTypeController extends AbstractEntityController {
