7135336de2202ee76fce83c8b033a5845c25d4cb
Jean Kahigiso Muchika <jkahigiso@gmail.com>
2012-10-03 18:08:26 +0200

    import hot fix

--- grails-app/services/org/chai/memms/maintenance/CorrectiveMaintenanceService.groovy
+++ grails-app/services/org/chai/memms/maintenance/CorrectiveMaintenanceService.groovy
@@ -36,8 +36,8 @@ import java.util.Set;
 import org.chai.location.LocationLevel;
 import org.chai.memms.Inventory
 import org.chai.memms.Inventories
-import org.chai.memms.correctiveMaintenance.CorrectiveMaintenance;
-import org.chai.memms.correctiveMaintenance.CorrectiveMaintenances;
+import org.chai.memms.CorrectiveMaintenance;
+import org.chai.memms.CorrectiveMaintenances;
 import org.chai.memms.maintenance.WorkOrder.Criticality;
 import org.chai.memms.maintenance.WorkOrder.OrderStatus;
 import org.chai.location.CalculationLocation;
