b85f1f7ec2b670e9f35b2f180482d80a242d10dd
Jean Kahigiso Muchika <jkahigiso@gmail.com>
2012-10-31 11:25:21 +0200

    fix manages dataLocation form and add test

---
+++ test/integration/org/chai/memms/location/LocationControllerSpec.groovy
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2012, Clinton Health Access Initiative.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.chai.memms.location
+
+import org.chai.location.Location;
+import org.chai.location.LocationLevel;
+import org.chai.memms.IntegrationTests;
+
+/**
+ * @author Jean Kahigiso M.
+ *
+ */
+class LocationControllerSpec extends IntegrationTests{
+	def locationController
+	
+	def "can create save a location"(){
+		setup:
+		setupLocationTree()
+		locationController =  new LocationController()
+		when:
+		def level = LocationLevel.findByCode(SECTOR)
+		def parent = Location.findByCode(BURERA)
+		locationController.params.code = CODE("123")
+		locationController.params.names_en = "Test Sector location"
+		locationController.params."level.id" =  level.id
+		locationController.params."parent.id" = parent.id
+		locationController.save()
+		then:
+		Location.count() == 6
+		Location.list()[5].code == CODE("123")
+		Location.list()[5].parent == parent
+	}
+
+}
