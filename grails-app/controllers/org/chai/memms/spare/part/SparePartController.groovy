ae4d73b0166b954aed0dd8e814e1f1e30750141e
Jean Kahigiso Muchika <jkahigiso@gmail.com>
2013-04-09 20:05:58 +0200

    quick fix of purchase date validation

--- grails-app/controllers/org/chai/memms/spare/part/SparePartController.groovy
+++ grails-app/controllers/org/chai/memms/spare/part/SparePartController.groovy
@@ -130,7 +130,7 @@ class SparePartController extends AbstractEntityController{
 
 	def validateEntity(def entity) {
 		boolean validStatus = true
-		if(entity.purchaseDate!=null && !(entity.purchaseDate.before(params.cmd.dateOfEvent) || entity.purchaseDate.compareTo(params.cmd.dateOfEvent)==0)){ 
+		if(entity.id==null && entity.purchaseDate!=null && !(entity.purchaseDate.before(params.cmd.dateOfEvent) || entity.purchaseDate.compareTo(params.cmd.dateOfEvent)==0)){ 
 			params.cmd.errors.rejectValue('dateOfEvent','date.of.event.before.purchase.date')
 			validStatus = (!params.cmd.hasErrors()) 
 			if(log.isDebugEnabled()) log.debug("Rejecting SparePartStatus: "+params.cmd.errors)
