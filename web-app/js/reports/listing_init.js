703953f1500eefa0c187392ff4a189e5511bd35f
Ilija Eftimov <ileeftimov@gmail.com>
2013-06-17 16:38:10 +0200

    fix horizontal scroll js

--- web-app/js/reports/listing_init.js
+++ web-app/js/reports/listing_init.js
@@ -1,64 +1,65 @@
 $(document).ready(function(){
 
-	// load listing stuff here
-	// ...
+  // load listing stuff here
+  // ...
 
-	// load jqueryui dialog box
-	$( "#js-customize-toggle").click(function(e) {
-	  e.preventDefault();
-		$( "#dialog-form" ).dialog({ modal: true });
-		$('.ui-dialog').resizable('destroy');
-	});
+  // load jqueryui dialog box
 
-	// load jqueryui horizontal scroll
-	var items, scroller = $('.v-tabs-subnav');
-	var width = 0;
-	var item = 0;
-	var scrolledWidth = 0;
-	scroller.children().each(function(){
-	    width += $(this).outerWidth(true);
-	});
-	scroller.css('width', width);
+  $( "#js-customize-toggle").click(function(e) {
+    e.preventDefault();
+    $( "#dialog-form" ).dialog({ modal: true });
+    $('.ui-dialog').resizable('destroy');
+  });
 
-	$(document).on('click', '.v-tabs-subnav-scroll-left', function(e){
-	  e.preventDefault();
-	  items = scroller.children();
-	  if(item > 0) {
-	    scrollRight(item);
-	    item -= 5;
-	  }
-	});
+  // load jqueryui horizontal scroll
+  var items, scroller = $('.v-tabs-subnav');
+  var width = 0;
+  var item = 0;
+  var scrolledWidth = 0;
+  scroller.children().each(function(){
+    width += $(this).outerWidth(true);
+  });
+  scroller.css('width', width);
 
-	$(document).on('click', '.v-tabs-subnav-scroll-right', function(e){
-	  e.preventDefault();
-	  items = scroller.children();
-	  if(item < (items.length-6)){
-	    scrollLeft(item);
-	    item += 5;
-	  }
-	});
+  $(document).on('click', '#js-scroll-left', function(e){
+    e.preventDefault();
+    items = scroller.children();
+    if(item > 0) {
+      scrollRight(item);
+      item -= 5;
+    }
+  });
 
-	function scrollLeft(item){
-	  var scrollWidth = 0
-	  items.each(function(idx){
-	    if(idx < 5){
-	      scrollWidth += $(items[idx]).outerWidth();
-	    }
-	  });
-	  scrolledWidth += scrollWidth;
-	  scroller.animate({'left' : (0 - scrolledWidth) + 'px'}, 'linear');
-	}
+  $(document).on('click', '#js-scroll-right', function(e){
+    e.preventDefault();
+    items = scroller.children();
+    if(item < (items.length-6)){
+      scrollLeft(item);
+      item += 5;
+    }
+  });
 
-	function scrollRight(item){
-	  var scrollWidth = 0
-	  items.each(function(idx){
-	    if(idx < 5){
-	      scrollWidth += $(items[idx]).outerWidth();
-	    }
-	  });
-	  scrolledWidth -= scrollWidth;
-	  scroller.animate({'left' : (0 - scrolledWidth) + 'px'}, 'linear');
-	}
+  function scrollLeft(item){
+    var scrollWidth = 0
+      items.each(function(idx){
+        if(idx < 5){
+          scrollWidth += $(items[idx]).outerWidth();
+        }
+      });
+    scrolledWidth += scrollWidth;
+    scroller.animate({'left' : (0 - scrolledWidth) + 'px'}, 'linear');
+  }
+
+  function scrollRight(item){
+    var scrollWidth = 0
+      items.each(function(idx){
+        if(idx < 5){
+          scrollWidth += $(items[idx]).outerWidth();
+        }
+      });
+    scrolledWidth -= scrollWidth;
+    scroller.animate({'left' : (0 - scrolledWidth) + 'px'}, 'linear');
+  }
 
 });
 
