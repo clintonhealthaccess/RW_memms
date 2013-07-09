7bf589c87bd8552e4e1c356b0958603e266d8551
aphrorwa <aphrorwa@gmail.com>
2013-07-09 16:38:45 +0200

    Report bugs related to the database deleted data fixed

--- grails-app/domain/org/chai/memms/inventory/Equipment.groovy
+++ grails-app/domain/org/chai/memms/inventory/Equipment.groovy
@@ -221,7 +221,7 @@ public class Equipment {
 		def today=new Date()
 		DateTime todayDateTime = new DateTime(today)
 		DateTime purchaseDateTime = new DateTime(purchaseDate)
-		if(expectedLifeTime!=null && purchaseDate!=null && purchaseCost!=null){	
+		if(id!=null && !id && expectedLifeTime!=null && purchaseDate!=null && purchaseCost!=null){	
 			def currentEquipmentLifeTime= (today-purchaseDate)
 			def currentEquipmentLifeTimeDays= currentEquipmentLifeTime.days
 			//C-((B/A)*C)
